03f55e19fcd29b432a63ffc878b2d08da9f43bb4
Shin Morisawa <git@tryh4rd.dev>
2026-02-28 20:42:56 +0900

    color.h is 100% feature complete

--- src/main.c
+++ src/main.c
@@ -2,45 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void rgb_handler(ColorRGB rgb) {
+    ColorRGB rgb2 = linear_to_srgb(rgb);
+    int r = rgb2.r * 255;
+    int g = rgb2.g * 255;
+    int b = rgb2.b * 255;
+    printf("\033[38;2;%d;%d;%dm", r, g, b);
+    printf("\033[48;2;%d;%d;%dm███████████████\n", r, g, b);
+}
+
 int main() {
-    ColorXYZ xyz = {
-        0.1804375,
-        0.0721750,
-        0.9503041
+    ColorOKLCH oklch_1 = {
+        0.7,
+        0.0,
+        0.0
     };
 
-    ColorLAB lab = {0};
-    ColorLCH lch = {0};
-    ColorOKLAB oklab = {0};
-    ColorOKLCH oklch = {0};
-    ColorXYZ output = {0};
+    ColorOKLCH oklch_2 = {
+        0.8,
+        0.3,
+        1.0
+    };
 
-    printf("start\n");
-    printf("x:%f y:%f z:%f\n", xyz.x, xyz.y, xyz.z);
+    ColorHandler handler = {0};
+    handler.color_rgb = rgb_handler;
+    handler.supported_spaces = RGB;
 
-    printf("doing XYZ -> LAB -> XYZ\n");
-    lab = color_xyz_to_lab(xyz); 
-    output = color_lab_to_xyz(lab);
-    printf("output: x:%f y:%f z:%f\n", output.x, output.y, output.z);
+    color_install_handler(handler);
 
-    printf("doing XYZ -> OKLAB -> XYZ\n");
-    oklab = color_xyz_to_oklab(xyz);
-    output = color_oklab_to_xyz(oklab);
-    printf("output: x:%f y:%f z:%f\n", output.x, output.y, output.z);
-
-    printf("doing XYZ -> LAB -> LCH -> LAB -> XYZ\n");
-    lab = color_xyz_to_lab(xyz);
-    lch = color_lab_to_lch(lab);
-    lab = color_lch_to_lab(lch);
-    output = color_lab_to_xyz(lab);
-    printf("output: x:%f y:%f z:%f\n", output.x, output.y, output.z);
-
-    printf("doing XYZ -> OKLab -> OKLch -> OKLab -> XYZ\n");
-    oklab = color_xyz_to_oklab(xyz);
-    oklch = color_oklab_to_oklch(oklab);
-    oklab = color_oklch_to_oklab(oklch);
-    output = color_oklab_to_xyz(oklab);
-    printf("output: x:%f y:%f z:%f\n", output.x, output.y, output.z);
+    for (double i = 0; i < 1.01; i += 0.001) {
+        ColorOKLCH oklch = color_oklch_lerp(oklch_1, oklch_2, i);
+        Color color = {0};
+        color.color_rgb = color_oklch_to_rgb(oklch);
+        color.type = RGB;
+        color_run_handler(0, color);
+    }
 
     return 0;
 }
