50452d583844d36a0cda7a15a5c16de92f3d0349
Shin Morisawa <git@tryh4rd.dev>
2026-03-20 12:49:48 +0900

    add f32 & f64

--- include/base.h
+++ include/base.h
@@ -14,6 +14,8 @@ typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
+typedef float f32;
+typedef double f64;
 
 #ifndef DEFAULT_ERROR_STRUCT
 #    define DEFAULT_ERROR_STRUCT
